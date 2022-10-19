aace83f357a316b2e18c01c15347c3f573a2c878
Pavel <KozhevnikovPA295@gmail.com>
2022-10-19 15:27:15 +0300

    Components created

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,7 +18,8 @@ export default function Layout({ initState }) {
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
           crossOrigin="anonymous"
         />
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous" />
         {/* скрипт наполнения вспомогательнго объекта initState для работы гидратации */}
         <script
           type="text/javascript"
