a46e559fc1f448f91d922a826071020b5cd9b159
Vadim Sidorov <vadim.06041974@gmail.com>
2022-10-19 15:18:17 +0300

    db init + fix some in serverjs

---
+++ src/db/migrations/20221019120406-create-album.js
@@ -0,0 +1,33 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Albums', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+      },
+      userid: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Albums');
+  },
+};
---
+++ src/db/models/photo.js
@@ -0,0 +1,26 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Photo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Album }) {
+      // define association here
+      this.belongsTo(Album, { foreignKey: 'albumid' });
+    }
+  }
+  Photo.init({
+    name: DataTypes.STRING,
+    albumid: DataTypes.INTEGER,
+    something: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Photo',
+  });
+  return Photo;
+};
